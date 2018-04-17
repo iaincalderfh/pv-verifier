1c12100b8f960d4fac7dc6eb675f7ac759fe3cf1
Iain Calder <iain.calder@fasthosts.com>
2018-04-17 12:18:25 +0100

    Run forever

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+trap finish TERM INT
+
+function finish {
+  echo "Exiting with code 0"
+  exit 0
+}
+
 if [ "$(ls -A $STORAGE_PATH)" ]; then
   # STORAGE_PATH is not empty, we expect to find it contains $TEST_FILE_NAME
   if [ ! -f "$STORAGE_PATH/$TEST_FILE_NAME" ]; then
@@ -8,11 +15,11 @@ if [ "$(ls -A $STORAGE_PATH)" ]; then
     exit 1
   else
     echo "Found expected test file $TEST_FILE_NAME"
-    exit 0
+    sleep infinity & wait
   fi
 else
   # STORAGE_PATH is empty
   echo "Storage path is empty.  Creating test file $TEST_FILE_NAME"
   touch "$STORAGE_PATH/$TEST_FILE_NAME"
-  exit 0
+  sleep infinity & wait
 fi
